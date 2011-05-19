3eb8bdd6a331674ce65e1fa673d5bc15a97c9e5b
Locojay <locojay@dev.dev>
2011-05-19 12:33:34 -0400

    bootsrap change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,20 @@
+f=$(pwd)
+#initialise and update submodules
 git submodule init
 git submodule update
-
+sudo brew install ack
+sudo pip install pysmell
+#ctags with Objc support
+git clone https://github.com/fishman/ctags.git /tmp/ctags-objc
+cd /tmp/ctags-objc
+autoheader
+autoconf
+./configure
+make
+sudo make install
+sudo rm -rf /tmp/ctags-obc
+#
+#command-t
+cd $f"/bundle/command-t/ruby/command-t"
+ruby extconf.rb
+make

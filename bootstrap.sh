19df9f5c144302862da28736559a53746873a99d
Locojay <locojay@dev.dev>
2011-05-24 10:27:40 -0400

    taglist adjustments

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,15 +7,8 @@ sudo easy_install pip
 sudo pip install pysmell
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
-#ctags with Objc support
-git clone https://github.com/fishman/ctags.git /tmp/ctags-objc
-cd /tmp/ctags-objc
-autoheader
-autoconf
-./configure
-make
-sudo make install
-sudo rm -rf /tmp/ctags-obc
+
+sudo brew install ctags
 #
 #command-t
 cd $f"/bundle/command-t/ruby/command-t"

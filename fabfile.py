c3198ecd253856867d3c2dc6c2e4b6b28c8c0814
LocJayMP <locojaymp@dev>
2011-07-07 23:06:50 -0400

    adding os type to fabric install

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,20 @@
 from fabric.api import local, lcd
 
-def _installdependencies():
-        local("sudo brew install ack")
-        local("sudo brew install ctags")
+def _installdependenciesosx():
+    local("sudo brew install ack")
+    local("sudo brew install ctags")
+
+def _installdependencieslinux():
+    local("sudo apt-get install ack")
+    local("sudo apt-get install ctags")
 
 def _installpythondependencies():
-        local("sudo pip install pysmell")
-        local("sudo pip install virtualenv")
-        local("sudo pip install virtualenvwrapper")
-        local("sudo pip install vim_debug")
-        local("sudo pip install ropevim")
-        local("sudo pip install pyflakes")
+    local("sudo pip install pysmell")
+    local("sudo pip install virtualenv")
+    local("sudo pip install virtualenvwrapper")
+    local("sudo pip install vim_debug")
+    local("sudo pip install ropevim")
+    local("sudo pip install pyflakes")
 
 def _installcommandt():
     with lcd("./bundle/command-t/ruby/command-t/"):
@@ -30,6 +34,12 @@ def install():
     local("git submodule update")
     _symlink()
     updatesubmodules()
-    _installdependencies()
+    ostype=local("uname -s",capture=True)
+    if ostype =="Darwin":
+        _installdependenciesosx()
+    elif ostype == "Linux":
+        _installdependencieslinux()
+    else:
+        raise Exception , "Unknow os type" #todo
     _installcommandt()
     _installpythondependencies()
